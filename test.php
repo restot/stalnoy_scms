018e86010226e5c9fdb225547e0f4d2fd2ca246a
restot <92ilya.icom@gmail.com>
2016-05-12 10:57:52 +0300

    neon remove

--- test.php
+++ test.php
@@ -1,10 +1,18 @@
 <?php
 
-// echo "string";
-require_once __DIR__.'/neon.php';
+// echo "string"; NOTE: neon test
+// require_once __DIR__.'/neon.php';
+//
+// $file=file_get_contents(__DIR__."/test.neon");
+// // var_dump($input);
+// $neon = new Neon;
+// $out=$neon->decode($file);
+// var_dump($out);
+//
 
-$file=file_get_contents(__DIR__."/test.neon");
-// var_dump($input);
-$neon = new Neon;
-$out=$neon->decode($file);
-var_dump($out);
+// NOTE: SetUp Class test
+
+require_once __DIR__.'/settings.php';
+
+// NOTE: work
+// var_dump(SetUP::db_host);
