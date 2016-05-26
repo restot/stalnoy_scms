43e0d18ce83d923d899ba535bcb7f93049a8eb84
restot <92ilya.icom@gmail.com>
2016-05-26 19:57:48 +0300

    some adds

--- scripts/fastsql.php
+++ scripts/fastsql.php
@@ -46,7 +46,7 @@ class fastsql
         $qu = $db->query("LOAD DATA LOCAL INFILE ?p INTO TABLE ?n FIELDS TERMINATED BY ',,' LINES TERMINATED BY '%%' ($cols)", '"'.str_replace('\\', '\\\\', $this->data_file).'"', $table);
         if ($qu) {
             echo 'Loading complite!'.EOL;
-            // unlink($this->data_file);
+            unlink($this->data_file);
             unset($this->data);
             unset($qu);
         }
--- scripts/upload.php
+++ scripts/upload.php
@@ -1,14 +1,30 @@
 <?php
-require_once  __DIR__.'/settings.php';
-require_once  __DIR__.'/safemysql.php';
+// require_once  __DIR__.'/settings.php';
+// require_once  __DIR__.'/safemysql.php';
 
-$db = new SafeMysql(array('user' => SetUp::db_user, 'pass' => SetUp::db_pass, 'db' => SetUp::db_database, 'charset' => 'utf8'));
+// $db = new SafeMysql(array('user' => SetUp::db_user, 'pass' => SetUp::db_pass, 'db' => SetUp::db_database, 'charset' => 'utf8'));
 
-Class UpdBase {
+class UpdBase
+{
+    public function prepareData($db)
+    {
+    }
 
-  public function prepareData()
-  {
-    # code...
-  }
-
+    public function getCOls(obj $db, string $table,array $filter=NULL)
+    {
+        $cols = $db->getCol('SELECT COLUMN_NAME
+	FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = DATABASE()
+	AND TABLE_NAME = ?s
+	ORDER BY ORDINAL_POSITION', $table);
+    $parseCol = '';
+        foreach ($cols as $c1 => $c2) {
+          if (in_array($filter,$c2)){
+            echo $c2." col ignored";
+            continue;
+          }
+            $parseCol.=$db->parse("?n,", $c2);
+        }
+$parseCol=substr($parseCol, 0, -1);
+return $parsCol;
+    }
 }
