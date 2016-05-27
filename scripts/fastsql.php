4772f2ba4d66e8d3fbdf95affe5a6a7b07ba26da
restot <92ilya.icom@gmail.com>
2016-05-27 12:19:54 +0300

    Задокументировал класс fastsql

--- scripts/fastsql.php
+++ scripts/fastsql.php
@@ -1,48 +1,89 @@
 <?php
 #Fast SQL
-
+/**
+* Класс для работы с Mysql базой данных.
+* необходим для быстрой загрузки больших массивов данных в базу.
+* осуществляется быстроействие за счет добавления запросов в файл, а он затем отправляется в базу.
+* быстродействие увеличивается минимум в х20 раз. даже объедененные запросы по 50к 100к или 500к символов,
+* в зависимости от того на какие запросы по обьему обеспеченна поддержка вашей базой данных, проигрывают по быстродействию
+* если выполнять запросы из файла.
+*
+* Пользуйтесь, старался restot.
+*
+* Класс SafeMySQL необходим для работы
+* @link http://phpfaq.ru/safemysql
+*
+* NOTE:P.s  перепроверяйте работу класса во время знакомства, класс очень дружелюбен к ошибкам, и грузит данные в любом виде...
+*/
 class fastsql
 {
-    // var $data_file=__DIR__."/data.txt";
-  // var $data='';
-    private $data_file = __DIR__."\data.txt";
-    private $data;
-    // private $string;
-// var $string;
-  // public function init() {
-  //   global $data;
-  //   global $data_file;
-  // $data_file=dirname(__FILE__)."/data.txt";
-  // $data='';
-  // }
+    private $data_file = __DIR__."\data.txt"; #создается файл для последующей отправки его в базу.
+    private $data; # образ файла в памяти, все информация в последствии добавляется в файл.
+
+/**
+* создает или в текущих запрос добавляет необходимые значения, только для INSERT MODE
+* @param (string) $value
+*/
+
   public function addCol(string $value)
   {
-      // global $data_file;
-      // global $string;
-      // var_dump($this->string);
-      // var_dump($string);
     $this->string .= $value.',,';
-      // var_dump($this->string);
   }
-    public function addRow()
+
+  /**
+  * Функция для разделения запросов, подготавливает условия для создания нового запроса только для INSERT MODE
+  * @param no args()
+  */
+  public function addRow()
     {
-        // global $string;
-    // global $data_file;
-    // global $data;
         $this->string = substr($this->string, 0, -2);
         $this->string .= '%%';
         $this->data .= $this->string;
-    // file_put_contents($data_file, $string, FILE_APPEND );
-    // var_dump($data_file);
-    unset($this->string);
+
+        unset($this->string);
     }
+/**
+ * Функция получает необходимые столбцы для отновления или вставки, необходима для UPDATE / INSERT MODEs.
+ * @param   SafeMySQL class($db) экземпляр класса
+ * @param   strind($table)
+ * @param   strind / array($filter)
+*/
+    public function getCOls( $db, string $table,$filter=NULL)
+    {
+
+        $cols = $db->getCol('SELECT COLUMN_NAME
+	         FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = DATABASE()
+	          AND TABLE_NAME = ?s
+	           ORDER BY ORDINAL_POSITION', $table);
+
+            $parseCol = '';
+
+            foreach ($cols as $c1 => $c2) {
+
+              if ( $filter!=NULL && in_array($c2,(is_array($filter)?$filter:(array)$filter))){
+                    echo $c2." col ignored\n";
+                    continue;
+                  }
+                  $parseCol.=$db->parse("?n,", $c2);
+                }
+
+                $parseCol=substr($parseCol, 0, -1);
+                return $parseCol;
+    }
+
+/*
+Функция подготавливает данные для записи в файл, и затем файл отправляется в базу и удаляется.
+
+@param  SafeMySQL class($db) экземпляр класса
+@param   strind($table)
+@param   strind($cols)
+*/
     public function sendData($db,$table,$cols)
     {
-        // global $data;
-    // global $data_file;
         $this->data = substr($this->data, 0, -2);
-        // var_dump($this->data_file);
+
         file_put_contents($this->data_file, $this->data);
+
         $qu = $db->query("LOAD DATA LOCAL INFILE ?p INTO TABLE ?n FIELDS TERMINATED BY ',,' LINES TERMINATED BY '%%' ($cols)", '"'.str_replace('\\', '\\\\', $this->data_file).'"', $table);
         if ($qu) {
             echo 'Loading complite!'.EOL;
