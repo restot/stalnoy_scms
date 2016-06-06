8682a153f020631ccfab1bb9a0a6ab31884eda9e
restot <92ilya.icom@gmail.com>
2016-06-06 21:52:46 +0300

    deploy

---
+++ scripts/settings_tmpl.php
@@ -0,0 +1,13 @@
+<?php
+
+Class SetUp
+
+{
+const db_host ='localhost';  //хост скули
+const db_user ='root';  //пользователь базы
+const db_database ='user';   //база
+const db_pass ='pass';   //пароль доступа
+}
+
+
+?>
