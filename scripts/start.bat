c1cf529fc3664405e69c561089235f4fbba43c88
restot <92ilya.icom@gmail.com>
2016-07-21 20:40:42 +0300

    fix

--- scripts/start.bat
+++ scripts/start.bat
@@ -2,5 +2,5 @@ echo on
 color a
 php init.php -normal
 php init.php -normal -stalnoy
-
+color
 pause
--- scripts/upd.bat
+++ scripts/upd.bat
@@ -2,4 +2,5 @@ echo on
 color e
 php init.php -force -stalnoy
 php init.php -force
+color
 pause
